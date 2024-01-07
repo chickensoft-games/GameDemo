8e897512037daf8c9c4f8b5b4a67cf1bb30258b1
Joanna May <joannamadisonmay@gmail.com>
2024-01-07 00:33:11 -0600

    test: update in game ui test

--- test/src/in_game_ui/state/InGameUILogicTest.cs
+++ test/src/in_game_ui/state/InGameUILogicTest.cs
@@ -8,6 +8,7 @@ using Shouldly;
 public class InGameUILogicTest : TestClass {
   private Mock<IInGameUI> _inGameUi = default!;
   private Mock<IAppRepo> _appRepo = default!;
+  private Mock<IGameRepo> _gameRepo = default!;
   private InGameUILogic _logic = default!;
 
   public InGameUILogicTest(Node testScene) : base(testScene) { }
@@ -16,14 +17,17 @@ public class InGameUILogicTest : TestClass {
   public void Setup() {
     _inGameUi = new Mock<IInGameUI>();
     _appRepo = new Mock<IAppRepo>();
+    _gameRepo = new Mock<IGameRepo>();
 
-    _logic = new InGameUILogic(_inGameUi.Object, _appRepo.Object);
+    _logic =
+      new InGameUILogic(_inGameUi.Object, _appRepo.Object, _gameRepo.Object);
   }
 
   [Test]
   public void Initializes() {
     _logic.Get<IInGameUI>().ShouldBe(_inGameUi.Object);
     _logic.Get<IAppRepo>().ShouldBe(_appRepo.Object);
+    _logic.Get<IGameRepo>().ShouldBe(_gameRepo.Object);
 
     _logic
       .GetInitialState()
