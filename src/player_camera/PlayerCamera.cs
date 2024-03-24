b69dee3b86e87a2cd114e6c268fb444ee8aacf91
Emerson Bottero <emersonbottero@gmail.com>
2024-03-23 23:27:33 -0300

    feat: Add joypad camera support

--- src/player_camera/PlayerCamera.cs
+++ src/player_camera/PlayerCamera.cs
@@ -135,10 +135,21 @@ public partial class PlayerCamera : Node3D, IPlayerCamera {
     CameraLogic.Start();
   }
 
-  public void OnPhysicsProcess(double delta)
-    => CameraLogic.Input(
+  public void OnPhysicsProcess(double delta) {
+    var xMotion = InputExtensions.GetJoyPadActionPressedMotion("camera_left", "camera_right", JoyAxis.RightX);
+    if (xMotion is not null) {
+      CameraLogic.Input(new PlayerCameraLogic.Input.JoyPadInputOccurred(xMotion));
+    }
+
+    var yMotion = InputExtensions.GetJoyPadActionPressedMotion("camera_up", "camera_down", JoyAxis.RightY);
+    if (yMotion is not null) {
+      CameraLogic.Input(new PlayerCameraLogic.Input.JoyPadInputOccurred(yMotion));
+    }
+
+    CameraLogic.Input(
       new PlayerCameraLogic.Input.PhysicsTicked(delta)
     );
+  }
 
   public override void _Input(InputEvent @event) {
     if (@event is InputEventMouseMotion motion) {
